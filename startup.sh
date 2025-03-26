5c3b9a6345aff6849efd89bf5051b4032fe443d3
Lailson Henrique <lailson.pi@gmail.com>
2025-03-26 04:21:06 -0300

    config raspberry

--- startup.sh
+++ startup.sh
@@ -8,6 +8,18 @@ cd $PROJECT_DIR
 # Iniciar log
 echo "$(date) - Iniciando script de inicialização" > $PROJECT_DIR/startup.log
 
+# Executar git pull para atualizar o código
+echo "$(date) - Verificando atualizações do repositório..." >> $PROJECT_DIR/startup.log
+git pull >> $PROJECT_DIR/startup.log 2>&1
+
+# Verificar se o git pull foi bem-sucedido
+if [ $? -eq 0 ]; then
+    echo "$(date) - Repositório atualizado com sucesso." >> $PROJECT_DIR/startup.log
+else
+    echo "$(date) - AVISO: Falha ao atualizar o repositório." >> $PROJECT_DIR/startup.log
+    # Continua mesmo se falhar, usando a versão atual do código
+fi
+
 # Ativar ambiente Conda
 source ~/miniconda3/bin/activate
 conda activate base
